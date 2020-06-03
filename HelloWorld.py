2a9fa4f287653c3e56ae8a0790c9add6bde62622
kriyanation <homeofmars@gmail.com>
2020-06-03 08:44:56 +0530

    LIkey

---
+++ HelloWorld.py
@@ -0,0 +1,35 @@
+a = 5; b = 10
+c = 2 ** 3
+inputString = 5 # input("Enter some stuff")
+print(inputString)
+print(c)
+y = c + int(inputString)
+print(y)
+
+x, y, z = 5, 6.5, 7
+print(z)
+
+
+def domist(bs):
+    print(bs * 2)
+    return 0
+
+
+domist(3)
+
+# list
+alist = [3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44]
+print(alist[1])
+print(alist[3:6])
+print(alist[4:])
+print(alist[7])
+alist[1] = 333333
+
+blist = (1, 2, 3, 5)
+#blist[0] = 233
+
+
+dlist = {1:"rtrt", "rerer":"tytyty"}
+print(dlist[1])
+print(dlist['rerer'])
+
